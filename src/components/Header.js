e138b6836f96d174fe26a0fac06093da80954e1f
jonathan-jablonski <jonathanjablonski94@gmail.com>
2021-06-28 12:28:08 -0500

    Header is fully functional

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+    // main logo and navbar
+    <header>
+      <h1>Jonathan Jablonski</h1>
+      <nav>
+        <a href="#about">About Me</a>
+        <a href="#work">Work</a>
+        <a href="#contact">Contact Me</a>
+        <a
+          href="../professional-portfolio/assets/images/Jonathan M. Jablonski Resume.pdf"
+          >Resume</a
+        >
+      </nav>
+    </header>
+    )
+}
